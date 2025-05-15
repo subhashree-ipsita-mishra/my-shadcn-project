ad1f31408d1f0fa5dc8d7b58e07cb73514904142
Your Name <your-email@example.com>
2025-05-15 10:32:56 +0530

    setup tailwind and alias with src

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindCss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindCss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
